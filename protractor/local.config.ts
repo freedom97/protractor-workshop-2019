cda2b80b1c6abd79b847855b0cbc76deef1e8d16
freedom97 <claudia.fernandez1@correo.icesi.edu.co>
2019-09-21 12:20:24 -0500

    delete comment again

--- protractor/local.config.ts
+++ protractor/local.config.ts
@@ -6,7 +6,6 @@ export const config: Config = {
   SELENIUM_PROMISE_MANAGER: false,
   framework: 'jasmine',
   specs: ['../test/google.spec.js'],
-  //seleniumAddress: 'http://localhost:4444/wd/hub',
   onPrepare: () => {
     browser.ignoreSynchronization = true;
     reporter();
