3ab76d9035835221e3ab906096cf05b365d05716
freedom97 <claudia.fernandez1@correo.icesi.edu.co>
2019-09-21 12:12:33 -0500

    edit google.spec.ts and local.config.ts

--- protractor/local.config.ts
+++ protractor/local.config.ts
@@ -3,9 +3,10 @@ import { reporter } from './helpers/reporter';
 
 
 export const config: Config = {
+  SELENIUM_PROMISE_MANAGER: false,
   framework: 'jasmine',
   specs: ['../test/google.spec.js'],
-  seleniumAddress: 'http://localhost:4444/wd/hub',
+  //seleniumAddress: 'http://localhost:4444/wd/hub',
   onPrepare: () => {
     browser.ignoreSynchronization = true;
     reporter();
